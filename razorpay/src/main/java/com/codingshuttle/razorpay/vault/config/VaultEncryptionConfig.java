b3030e7bff167f9874b1544efb29a774a0a00d13
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-09-03 18:00:00 +0530

    Add files via upload

---
+++ razorpay/src/main/java/com/codingshuttle/razorpay/vault/config/VaultEncryptionConfig.java
@@ -0,0 +1,23 @@
+package com.codingshuttle.razorpay.vault.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Configuration
+public class VaultEncryptionConfig {
+
+
+    public static BytesEncryptor panEncrypter(byte[] dek) {
+        SecretKeySpec decKey = new SecretKeySpec(dek, "AES");
+        return new AesBytesEncryptor(decKey, KeyGenerators.secureRandom(12),
+                AesBytesEncryptor.CipherAlgorithm.GCM);
+    }
+
+}
---
+++ razorpay/src/main/java/com/codingshuttle/razorpay/vault/controller/VaultController.java
@@ -0,0 +1,31 @@
+package com.codingshuttle.razorpay.vault.controller;
+
+import com.codingshuttle.razorpay.merchant.security.MerchantContext;
+import com.codingshuttle.razorpay.vault.dto.request.TokenizeRequest;
+import com.codingshuttle.razorpay.vault.dto.response.TokenizeResponse;
+import com.codingshuttle.razorpay.vault.service.VaultService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/vault")
+public class VaultController {
+
+    private final VaultService vaultService;
+    private final MerchantContext merchantContext;
+
+    @PostMapping("/tokenize")
+    public ResponseEntity<TokenizeResponse> tokenize(@Valid @RequestBody TokenizeRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(vaultService.tokenize(request, merchantContext.getMerchantId()));
+    }
+}
