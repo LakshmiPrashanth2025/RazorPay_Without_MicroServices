8d0a1c40b967b3651c6321f9a985cf28b351e648
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-09-03 17:53:29 +0530

    Add files via upload

---
+++ razorpay/src/main/java/com/codingshuttle/razorpay/common/enums/EventAggregateType.java
@@ -0,0 +1,8 @@
+package com.codingshuttle.razorpay.common.enums;
+
+public enum EventAggregateType {
+    PAYMENT,
+    ORDER,
+    REFUND,
+    SETTLEMENT
+}
---
+++ razorpay/src/main/java/com/codingshuttle/razorpay/common/enums/OutboxStatus.java
@@ -0,0 +1,7 @@
+package com.codingshuttle.razorpay.common.enums;
+
+public enum OutboxStatus {
+    PENDING,
+    PUBLISHED,
+    FAILED
+}
